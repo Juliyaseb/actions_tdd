84a520eb3755919afa4e5f54061337dadffca1f9
Juliya Sebastian <juliyasebastian001@gmail.com>
2026-03-11 14:13:26 +0100

    changed substract logic

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ class Calculator:
 
     def subtract(self, a, b):
         """Return the difference of a and b."""
-        return a - b
+        return a + b
 
     def multiply(self, a, b):
         """Return the product of a and b."""
@@ -29,3 +29,5 @@ class Calculator:
         if not numbers:
             raise ValueError("The list of numbers cannot be empty")
         return sum(numbers) / len(numbers)
+   
+    
